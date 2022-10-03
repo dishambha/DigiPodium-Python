703793ef2066c5ce2e9b3e40fc7d01d8a00471c3
dishambha awasthi <dishambhaawasthi0131@gmail.com>
2022-10-04 00:28:49 +0530

    data analysis

---
+++ function_n_modules/ex10.py
@@ -0,0 +1,2 @@
+info = "My name is Dishambha Awasthi,I am a student of BBDU"
+print(info.split(","))
---
+++ function_n_modules/test.py
@@ -0,0 +1,18 @@
+# class tester:
+#     def __init__(self, id):
+#         self.id = str(id)
+#         id="333"
+ 
+# temp = tester(33)
+# print(temp.id)
+
+# x = "python"
+# i = "i"
+# while i in x:
+#     print(i, end=" ")
+i = 1
+while True:
+    if i%0O7 == 0:
+        break
+    print(i)
+    i -= 1
