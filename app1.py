8ac70b8d05982c6ad415059f1ec4a975d69fcaaa
dishambha awasthi <dishambhaawasthi0131@gmail.com>
2022-11-16 23:39:23 +0530

    Create app1.py

---
+++ app1.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+st.set_page_config(
+    page_title="STUDENTS GUID",
+    page_icon="😎",
+    layout="wide"
+)
+
+st.title("GET ALL YOUR NOTES HERE")
+st.info("THIS APP IS BUILD TO KEEP ALL NOTES AND ASSIGNMENT AT ONE PLACE")
