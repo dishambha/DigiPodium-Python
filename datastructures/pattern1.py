fe32f2a727a7ff9069696e698ae426d004826438
dishambha awasthi <dishambhaawasthi0131@gmail.com>
2022-12-10 00:58:58 +0530

    practice

---
+++ datastructures/pattern1.py
@@ -0,0 +1,6 @@
+for i in range (1,5):
+    print("*"*i)
+    i=i+1
+for j in range(5,0,-1): 
+    print("*" *j)
+    j=j-1
---
+++ patterns/pattern1.py
@@ -0,0 +1,16 @@
+i=6
+j=6
+k=0
+while i>0 and j > 0:
+    print("*"*i,(" "*k),"*"*j)
+    i -=1
+    j -=1
+    k += 2
+a=2
+b=2
+c=8
+while a<7 and b<7 :
+    print("*"*a," "*c,"*"*b)
+    a += 1
+    b+=1
+    c -=2
